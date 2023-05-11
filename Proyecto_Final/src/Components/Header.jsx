e3f57e56e632499952fc0dab0583e3a0d1d412b1
Noemi Mendoza <116498194+Mendozanoe@users.noreply.github.com>
2023-05-10 22:34:20 -0300

    1er commit

---
+++ Proyecto_Final/src/Components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import img from "../assets/logo.png"
+
+const Header = () => {
+  return (
+    <header>
+         <div className="header-container">
+        <div className="header-left">
+        <img src={img} alt="Logo de la empresa" className='logotipo'/>
+          <h1 style={{ fontSize: "24px" }}>El lema de la empresa</h1>
+          {/* Aquí puedes agregar el logotipo y el lema de la empresa */}
+        </div>
+        <div className="header-right">
+          <button>Crear cuenta</button>
+          <button>Iniciar sesión</button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
