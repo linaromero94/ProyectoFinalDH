d88e5e10a4a39eada5d3f21549fef7db26c1f6e9
Noemi Mendoza <116498194+Mendozanoe@users.noreply.github.com>
2023-05-11 12:54:02 -0300

    2Actualizacion Header"

--- Proyecto_Final/src/Components/Header.jsx
+++ Proyecto_Final/src/Components/Header.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import img from "../assets/logo.png"
+import img from "../assets/logo3.png"
 
 const Header = () => {
   return (
     <header>
          <div className="header-container">
-        <div className="header-left">
-        <img src={img} alt="Logo de la empresa" className='logotipo'/>
-          <h1 style={{ fontSize: "24px" }}>El lema de la empresa</h1>
-          {/* Aquí puedes agregar el logotipo y el lema de la empresa */}
-        </div>
+        
+          <a className="header-left" href="/">
+            
+         <img src={img} alt="Logo de la empresa" className='logotipo'/>
+          <h1 style={{color:"#1a1a1a", fontSize: "24px", fontFamily:"serif",paddingTop:"30px",paddingLeft:"15px", 
+          textShadow:"1px 1px 1px rgba(0, 0, 0, 0.2" }}
+          >Sabores que inspiran... </h1>
+          </a>
+       
+        
+       
         <div className="header-right">
           <button>Crear cuenta</button>
           <button>Iniciar sesión</button>
